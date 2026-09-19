eeb58ba1accfaa461d8c4eb5957ae5e4bffdfb21
João Pedro <jplisboa.dev@gmail.com>
2026-09-19 18:56:32 -0300

    feat: add url in Service

--- PlayerRadio/src/main/java/com/example/PlayerRadio/service/service.java
+++ PlayerRadio/src/main/java/com/example/PlayerRadio/service/service.java
@@ -1,5 +1,15 @@
 package com.example.PlayerRadio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.PlayerRadio.config.ApiConfig;
+
+@Service 
 public class service {
+
+  @Autowired
+  ApiConfig apiConfig; 
+  String url = apiConfig.getSearchUrl() + "?country=Brazil&state=Minas Gerais&city=Belo Horizonte";
   
 }
